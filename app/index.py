386b933b4690b7d663ffa9cffbc49fa0ace362da
Rakshya <moktanrakshya1@gmail.com>
2024-02-28 14:52:23 +0700

    Complete A5

--- app/index.py
+++ app/index.py
@@ -12,17 +12,20 @@ from Encoder_layer import EncoderLayer
 from Feed_foreward import PositionwiseFeedforwardLayer
 from Additive_attention import AdditiveAttention
 from Mutihead_attention import MultiHeadAttentionLayer
+from Bert import BERT, calculate_similarity
 from torchtext.data.utils import get_tokenizer
 from nepalitokenizers import WordPiece
 from torch.nn.utils.rnn import pad_sequence
 from PyPDF2 import PdfReader
-import fitz 
+# import fitz 
 import io
 import pandas as pd
 import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 from spacy.matcher import Matcher
+from sklearn.metrics.pairwise import cosine_similarity
 import csv
+from transformers import BertTokenizer
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -75,16 +78,55 @@ lstm_model.load_state_dict(torch.load(model_path_2, map_location=device))
 previous_queries = []
 token_transform = {}
 
-# Specify the file path from where you want to load the vocab_transform
-vocab_transform_path = '../Jupyter Files/model/vocab'
+# # Specify the file path from where you want to load the vocab_transform
+# vocab_transform_path = '../Jupyter Files/model/vocab'
 
-# Load the vocab_transform using pickle
-vocab_transform = torch.load(vocab_transform_path)
+# # Load the vocab_transform using pickle
+# vocab_transform = torch.load(vocab_transform_path)
 
-nlp = spacy.load('en_core_web_md')
-skill_path = '../Jupyter Files/data/skills.jsonl'
-ruler = nlp.add_pipe("entity_ruler")
-ruler.from_disk(skill_path)
+# nlp = spacy.load('en_core_web_md')
+# skill_path = '../Jupyter Files/data/skills.jsonl'
+# ruler = nlp.add_pipe("entity_ruler")
+# ruler.from_disk(skill_path)
+
+# load the model and all its hyperparameters
+save_path = '../Jupyter Files/model/bert.pt'
+params, state = torch.load(save_path)
+model_bert = BERT(**params, device=device).to(device)
+model_bert.load_state_dict(state)
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+# define mean pooling function
+def mean_pool(token_embeds, attention_mask):
+    # reshape attention_mask to cover 768-dimension embeddings
+    in_mask = attention_mask.unsqueeze(-1).expand(
+        token_embeds.size()
+    ).float()
+    # perform mean-pooling but exclude padding tokens (specified by in_mask)
+    pool = torch.sum(token_embeds * in_mask, 1) / torch.clamp(
+        in_mask.sum(1), min=1e-9
+    )
+    return pool
+
+def configurations(u,v):
+    # build the |u-v| tensor
+    uv = torch.sub(u, v)   # batch_size,hidden_dim
+    uv_abs = torch.abs(uv) # batch_size,hidden_dim
+
+    # concatenate u, v, |u-v|
+    x = torch.cat([u, v, uv_abs], dim=-1) # batch_size, 3*hidden_dim
+    return x
+
+def cosine_similarity(u, v):
+    dot_product = np.dot(u, v)
+    norm_u = np.linalg.norm(u)
+    norm_v = np.linalg.norm(v)
+    similarity = dot_product / (norm_u * norm_v)
+    return similarity
+
+
+
 
 def preprocessing(sentence):
     stopwords    = list(STOP_WORDS)
@@ -227,15 +269,16 @@ def tensor_transform(token_ids):
     return torch.cat((torch.tensor([2]), torch.tensor(token_ids), torch.tensor([2])))
 
 
-# src and trg language text transforms to convert raw strings into tensors indices
-text_transform = {}
-token_transform["en"] = get_tokenizer('spacy', language='en_core_web_sm')
-token_transform["ne"] = WordPiece()
-for ln in [SRC_LANGUAGE, TRG_LANGUAGE]:
-    text_transform[ln] = sequential_transforms(token_transform[ln], #Tokenization
-                                               vocab_transform[ln], #Numericalization
-                                               tensor_transform)
+# # src and trg language text transforms to convert raw strings into tensors indices
+# text_transform = {}
+# token_transform["en"] = get_tokenizer('spacy', language='en_core_web_sm')
+# token_transform["ne"] = WordPiece()
+# for ln in [SRC_LANGUAGE, TRG_LANGUAGE]:
+#     text_transform[ln] = sequential_transforms(token_transform[ln], #Tokenization
+#                                                vocab_transform[ln], #Numericalization
+#                                                tensor_transform)
 
+max_seq_length = 1024
 
 # Route for the home page
 @app.route('/')
@@ -360,64 +403,96 @@ def a3():
     # Render the translation result in the HTML template
     return render_template('pages/a3.html', translation_result=translation_result, previous_queries=previous_queries)
 
-@app.route('/a4', methods=['GET', 'POST'])
-def a4():
-    results=[]
-    if request.method == 'POST':
-        # Handle file upload
-        uploaded_files = request.files.getlist('fileInput')
-        for file in uploaded_files:
-            # Handle file upload
-            # Handle file upload
-            pdf_content = file.read()
-            pdf_document = fitz.open(stream=io.BytesIO(pdf_content), filetype="pdf")
-            if pdf_document:
-            # Process the uploaded PDF file
-                text = ""
-                for page_num in range(pdf_document.page_count):
-                    page = pdf_document[page_num]
-                    text += page.get_text()
-                text = preprocessing(text)
-                result = get_entities(text)
-                results.append(result)
-                print("Results",results)
-                matcher = Matcher(nlp.vocab)
-                pattern = [
-                    {"POS": "PROPN",  # person's name should be a proper noun
-                    "OP": "{2}",  # person's name usually consists of 2 parts; first name and last name (in some scenario, 3 if a person has middle name)
-                    "ENT_TYPE": "PERSON"  # person's name is of 'PERSON' entity type|
-                    },
-                ]
-                matcher.add("PERSON NAME", [pattern], greedy="LONGEST")
-                doc = nlp(text)
-                matches = matcher(doc)
-                matches.sort(key = lambda x: x[1])
 
-                person_names = []
+# @app.route('/a4', methods=['GET', 'POST'])
+# def a4():
+#     results=[]
+#     if request.method == 'POST':
+#         # Handle file upload
+#         uploaded_files = request.files.getlist('fileInput')
+#         for file in uploaded_files:
+#             # Handle file upload
+#             # Handle file upload
+#             pdf_content = file.read()
+#             pdf_document = fitz.open(stream=io.BytesIO(pdf_content), filetype="pdf")
+#             if pdf_document:
+#             # Process the uploaded PDF file
+#                 text = ""
+#                 for page_num in range(pdf_document.page_count):
+#                     page = pdf_document[page_num]
+#                     text += page.get_text()
+#                 text = preprocessing(text)
+#                 result = get_entities(text)
+#                 results.append(result)
+#                 print("Results",results)
+#                 matcher = Matcher(nlp.vocab)
+#                 pattern = [
+#                     {"POS": "PROPN",  # person's name should be a proper noun
+#                     "OP": "{2}",  # person's name usually consists of 2 parts; first name and last name (in some scenario, 3 if a person has middle name)
+#                     "ENT_TYPE": "PERSON"  # person's name is of 'PERSON' entity type|
+#                     },
+#                 ]
+#                 matcher.add("PERSON NAME", [pattern], greedy="LONGEST")
+#                 doc = nlp(text)
+#                 matches = matcher(doc)
+#                 matches.sort(key = lambda x: x[1])
 
-                for match in matches:
-                    person_names.append((str(doc[match[1]:match[2]]),
-                                        nlp.vocab.strings[match[0]]))
+#                 person_names = []
 
-                person_names = list(set(person_names))
-                matcher.add("PERSON", [[{"POS": "PROPN", "OP": "{2}", "ENT_TYPE": "PERSON"}]], greedy="LONGEST")
-                matcher.add("EMAIL", [[{"LIKE_EMAIL": True}]], greedy="LONGEST")
-                matcher.add("URL", [[{"LIKE_URL": True}]], greedy="LONGEST")
-                matcher.add("PHONE NUMBER", [
-                    [{"ORTH": {"in": ["(", "["]}, "is_digit": True}, {"SHAPE": "dddd"}, {"ORTH": {"in": [")", "]"]}}, {"SHAPE": "dddd"}, {"SHAPE": "dddd"}],
-                    [{"ORTH": {"in": ["(", "["]}, "is_digit": True}, {"SHAPE": "ddd"}, {"ORTH": {"in": [")", "]"]}}, {"SHAPE": "ddd"}, {"SHAPE": "dddd"}],
-                    [{"SHAPE": "ddd"}, {"ORTH": "-"}, {"SHAPE": "ddd"}, {"ORTH": "-"}, {"SHAPE": "dddd"}],
-                    [{"SHAPE": "ddd"}, {"SHAPE": "ddd"}, {"SHAPE": "dddd"}],
-                    ])
+#                 for match in matches:
+#                     person_names.append((str(doc[match[1]:match[2]]),
+#                                         nlp.vocab.strings[match[0]]))
 
-                print(results)
-                extracted_info = result  # Assign the results list to extracted_info
+#                 person_names = list(set(person_names))
+#                 matcher.add("PERSON", [[{"POS": "PROPN", "OP": "{2}", "ENT_TYPE": "PERSON"}]], greedy="LONGEST")
+#                 matcher.add("EMAIL", [[{"LIKE_EMAIL": True}]], greedy="LONGEST")
+#                 matcher.add("URL", [[{"LIKE_URL": True}]], greedy="LONGEST")
+#                 matcher.add("PHONE NUMBER", [
+#                     [{"ORTH": {"in": ["(", "["]}, "is_digit": True}, {"SHAPE": "dddd"}, {"ORTH": {"in": [")", "]"]}}, {"SHAPE": "dddd"}, {"SHAPE": "dddd"}],
+#                     [{"ORTH": {"in": ["(", "["]}, "is_digit": True}, {"SHAPE": "ddd"}, {"ORTH": {"in": [")", "]"]}}, {"SHAPE": "ddd"}, {"SHAPE": "dddd"}],
+#                     [{"SHAPE": "ddd"}, {"ORTH": "-"}, {"SHAPE": "ddd"}, {"ORTH": "-"}, {"SHAPE": "dddd"}],
+#                     [{"SHAPE": "ddd"}, {"SHAPE": "ddd"}, {"SHAPE": "dddd"}],
+#                     ])
+
+#                 print(results)
+#                 extracted_info = result  # Assign the results list to extracted_info
 
 
-                return render_template('pages/a4.html', extracted_info=extracted_info)
+#                 return render_template('pages/a4.html', extracted_info=extracted_info)
             
 
-    return render_template('pages/a4.html', extracted_info=None)
+#     return render_template('pages/a4.html', extracted_info=None)
+
+@app.route('/a5', methods=['POST', 'GET'])
+def a5():
+    # Initialize translation result
+    similarity = None
+
+    # Check if the request method is POST
+    if request.method == "POST":
+        # Get the input sentence from the form
+        input_sentence_1 = request.form['prompt_a5_1']
+        input_sentence_2 = request.form['prompt_a5_2']
+
+        # Check if the input sentence is not empty
+        if input_sentence_1 and input_sentence_2:
+            # Load the pre-trained Seq2Seq model
+            load_path = '../Jupyter Files/model/bert.pt'
+            params, state = torch.load(load_path)
+            model_bert = BERT(**params, device=device).to(device)
+            model_bert.load_state_dict(state)
+
+            # Print the input sentence
+            print(input_sentence_1)
+            print(input_sentence_2)
+
+            score = calculate_similarity(model_bert, tokenizer, params['max_len'], input_sentence_1, input_sentence_2, device)
+
+            # Save the input sentence to the list of previous queries
+            previous_queries.append((input_sentence_1, input_sentence_2))
+
+    # Render the translation result in the HTML template
+    return render_template('pages/a5.html', generated_scores=score, previous_queries=previous_queries)
 
 
     
